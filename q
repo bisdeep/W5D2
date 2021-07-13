e58ab40d91db1879b547ce3f48a0c80816aa4508
mordes89 <mordes89@gmail.com>
2021-07-13 17:38:11 -0400

    end of day

---
+++ q
@@ -0,0 +1,150 @@
+   continent   
+---------------
+ Europe
+ Europe
+ Africa
+ Americas
+ Europe
+ Asia-Pacific
+ Europe
+ Europe
+ Americas
+ Middle East
+ Americas
+ Europe
+ Europe
+ Americas
+ Africa
+ South Asia
+ South America
+ Europe
+ Africa
+ Asia-Pacific
+ Europe
+ Africa
+ Africa
+ Asia-Pacific
+ Africa
+ Africa
+ Africa
+ Africa
+ South America
+ Africa
+ Americas
+ Europe
+ Americas
+ Europe
+ Europe
+ Europe
+ Africa
+ Americas
+ Americas
+ Asia-Pacific
+ South America
+ Americas
+ Africa
+ Africa
+ Europe
+ Asia-Pacific
+ Europe
+ Europe
+ Africa
+ Europe
+ Africa
+ Europe
+ Americas
+ Americas
+ Africa
+ Africa
+ South America
+ Americas
+ Americas
+ Europe
+ Europe
+ Europe
+ Middle East
+ Africa
+ Americas
+ Middle East
+ Asia-Pacific
+ Asia-Pacific
+ Middle East
+ Asia-Pacific
+ Asia-Pacific
+ Europe
+ Middle East
+ Africa
+ Africa
+ Africa
+ Europe
+ Europe
+ Europe
+ Africa
+ Africa
+ Africa
+ Europe
+ Asia-Pacific
+ Middle East
+ Africa
+ Asia-Pacific
+ Europe
+ Europe
+ Asia-Pacific
+ Africa
+ Africa
+ Asia-Pacific
+ Asia-Pacific
+ Americas
+ Africa
+ Asia-Pacific
+ Europe
+ Middle East
+ Asia-Pacific
+ Americas
+ Asia-Pacific
+ South America
+ Europe
+ Middle East
+ Africa
+ Europe
+ Africa
+ Asia-Pacific
+ Europe
+ Africa
+ Africa
+ Europe
+ Africa
+ Africa
+ Asia-Pacific
+ Europe
+ Europe
+ Asia-Pacific
+ Africa
+ South Asia
+ Americas
+ Americas
+ Americas
+ South America
+ Africa
+ Europe
+ Europe
+ Middle East
+ Asia-Pacific
+ Asia-Pacific
+ Africa
+ South Asia
+ Europe
+ Africa
+ Asia-Pacific
+ Americas
+ Middle East
+ Asia-Pacific
+ Asia-Pacific
+ Middle East
+ South America
+ Asia-Pacific
+ Middle East
+ Africa
+ Africa
+(146 rows)
+
--- sqlZoo/skeleton/lib/04_select_within_select.rb
+++ sqlZoo/skeleton/lib/04_select_within_select.rb
@@ -35,6 +35,12 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE population >
+                (SELECT population 
+                FROM countries
+                WHERE name = 'Russia')
   SQL
 end
 
@@ -42,6 +48,12 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE continent = 'Europe' AND gdp/population >
+        (SELECT gdp/population
+        FROM countries
+        WHERE name = 'United Kingdom')
   SQL
 end
 
@@ -49,6 +61,12 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT name, continent
+  FROM countries
+  WHERE continent IN
+      (SELECT continent
+      FROM countries
+      WHERE name = 'Belize' OR name = 'Belgium')
   SQL
 end
 
@@ -56,14 +74,36 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT name, population
+  FROM countries
+  WHERE population > 
+      -- Canada pop
+      (SELECT population
+      FROM countries
+      WHERE name = 'Canada')
+  AND population <
+      -- Poland population
+      (SELECT population
+      FROM countries
+      WHERE name = 'Poland')
   SQL
 end
 
 def sparse_continents
-  # Find every country that belongs to a continent where each country's
+  # Find every country that belongs to a 1)continent where each country's
   # population is less than 25,000,000. Show name, continent and
   # population.
+
+  # Show name, continent and population of countries that belong to a continent...
+  # where each country's population is less than 25,000,000
+  
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT name, continent, population
+  FROM countries
+  WHERE population < 25000000 AND continent IN
+    (SELECT continent
+    FROM countries
+    WHERE <= ALL (population < 25000000);)  
   SQL
 end
